6949dc7d4e12b01786531ac68fad03b8fb2f06f3
Ridwan Arifandi <orangerdigiart@gmail.com>
2025-07-06 20:15:19 +0700

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'blaze-commerce/gutenberg',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '74fb4e33029d452777a11cb5753fa2fe4ac2794c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'blaze-commerce/gutenberg' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '74fb4e33029d452777a11cb5753fa2fe4ac2794c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
